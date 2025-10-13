42ce519a920bcefc1540ab613431e0ac0394d520
CarlsdeLeon <124699853+CarlsdeLeon@users.noreply.github.com>
2025-10-12 21:46:19 -0600

    test: create connection tests and confirm backend services are working

--- secret-app/backend/secret_proj/settings.py
+++ secret-app/backend/secret_proj/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'secrets_app',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'secret_proj.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
